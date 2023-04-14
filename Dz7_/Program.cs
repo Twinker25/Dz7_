0c885c3b40df7f81ea05b4ef900b1cee37e6336f
Twinker25 <shashokd@gmail.com>
2023-04-14 17:56:34 +0300

    Add project files.

---
+++ Dz7_/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Dz7_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ch;
+            do
+            {
+                Console.Write("Enter task (1 - 3): ");
+                ch = int.Parse(Console.ReadLine());
+                switch (ch) 
+                {
+                    case 1:
+                        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+                        Array myArray = new Array(arr);
+                        Console.Write("Enter sort number for less element array: ");
+                        int less = int.Parse(Console.ReadLine());
+                        Console.Write("Enter sort number for greater element array: ");
+                        int greater = int.Parse(Console.ReadLine());
+                        Console.WriteLine($"Number of values less than {less}: " + myArray.Less(less));
+                        Console.WriteLine($"Number of values greater than {greater}: " + myArray.Greater(greater));
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+
+                        break;
+                    default: 
+                        Console.WriteLine("Error! Try again!"); 
+                        break;
+                }
+            } while (ch < 1 || ch > 3); 
+        }
+    }
+}
---
+++ Dz7_/Task1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dz7_
+{
+    interface ICalc
+{
+    int Less(int valueToCompare);
+    int Greater(int valueToCompare);
+}
+
+class Array : ICalc
+{
+    private int[] arr;
+
+    public Array(int[] arr)
+    {
+        this.arr = arr;
+    }
+
+    public int Less(int valueToCompare)
+    {
+        int count = 0;
+        foreach (int num in arr)
+        {
+            if (num < valueToCompare)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int Greater(int valueToCompare)
+    {
+        int count = 0;
+        foreach (int num in arr)
+        {
+            if (num > valueToCompare)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+}
